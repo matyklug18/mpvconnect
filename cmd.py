b7344649a18df82497be32684df6bc2e5169dd83
matyklug18 <klugmathias@gmail.com>
2021-05-08 04:53:53 +0200

    initial commit

---
+++ cmd.py
@@ -0,0 +1,31 @@
+import readline
+from mpvconnect import MPVConnection as MPV
+
+mpv = MPV(conn_type="network")
+mpv.connect(host="10.0.0.23", timeout=0.1)
+
+while True:
+    cmd = input("> ")
+    if cmd.startswith("p"):   # pause             
+        mpv.set_pause(not mpv.get_pause())
+    elif cmd.startswith("v"): # volume
+        volume = int(mpv.get_volume())
+        if len(cmd) > 1:
+            volume = int(eval(cmd[1:], {'v':volume}, {}))
+            mpv.set_volume(volume)
+        print(f"{volume}%")
+    elif cmd.startswith("l"): # playlist
+        playlist = int(mpv.get_playlist())
+        if len(cmd) > 1:
+            playlist = int(eval(cmd[1:], {'l':playlist}, {}))
+            mpv.set_playlist(playlist)
+        print(f"{playlist}.")
+    elif cmd.startswith("s"): # playback (seek)
+        playback = mpv.get_playback()
+        if len(cmd) > 2:
+            result = eval(cmd[2:], {'t':playback["time"], 'p':playback["percent"], 'm':playback["max"]}, {})
+            if cmd[1] == 't':
+                mpv.set_playback_time(result)
+            elif cmd[1] == 'p':
+                mpv.set_playback_percent(result)
+        print(f't:{playback["time"]}s / m:{playback["max"]}s (p:{playback["percent"]}%)')
---
+++ mpvconnect.py
@@ -0,0 +1,79 @@
+import socket
+import os
+import json
+
+class MPVConnection:
+    def __init__(self, conn_type="direct"):
+        self.__conn_type = conn_type
+        if conn_type == "direct":
+            self.__socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        elif conn_type == "network":
+            self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def connect(self, socket_file="./mpv-ipc.socket", timeout=0.005, host="127.0.0.1", port=4895):
+        if self.__conn_type == "direct":
+            self.__socket.connect(os.path.abspath(socket_file))
+        elif self.__conn_type == "network":
+            self.__socket.connect((host, port))
+
+        self.__socket.settimeout(timeout)
+
+    def disconnect(self):
+        self.__socket.close()
+
+    def __send_cmd(self, cmd):
+        self.__socket.send(f'{cmd}\n'.encode("utf-8"))
+
+        buff = ''
+
+        while True:
+            try:
+                data = self.__socket.recv(1024)
+            except socket.timeout as e:
+                break
+            buff += data.decode("utf-8")
+        return buff
+
+    def __get_prop(self, prop):
+        try:
+            return json.loads(self.__send_cmd(f'{{ "command": ["get_property", "{prop}"] }}'))["data"]
+        except json.decoder.JSONDecodeError:
+            return self.__get_prop(prop)
+
+    def __set_prop(self, prop, value):
+        str_val = value
+        if type(value) == bool:
+            str_val = "true" if value else "false"
+        if type(value) == str:
+            str_val = f'"{value}"'
+        self.__send_cmd(f'{{ "command": ["set_property", "{prop}", {str_val}] }}')
+
+    def get_volume(self):
+        return self.__get_prop("volume")
+
+    def set_volume(self, value):
+        self.__set_prop("volume", value)
+
+    def get_pause(self):
+        return self.__get_prop("pause")
+
+    def set_pause(self, value):
+        self.__set_prop("pause", value)
+
+    def get_playback(self):
+        return {"time":self.__get_prop("time-pos"), "max":self.__get_prop("duration"), "percent":self.__get_prop("percent-pos")}
+
+    def set_playback_time(self, value):
+        self.__set_prop("time-pos", value)
+
+    def set_playback_percent(self, value):
+        self.__set_prop("percent-pos", value)
+
+    def get_playlist(self):
+        return self.__get_prop("playlist-pos")
+
+    def set_playlist(self, value):
+        self.__set_prop("playlist-pos", value)
+
+
+
